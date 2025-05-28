65c115931b7f60e2f7195720d1b55f78b4f77343
LeoLetellier <leo.letellier.6@gmail.com>
2025-05-28 13:41:51 +0200

    Add command to check aspeo version

---
+++ aspeo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Check ASPeo version
+
+Usage:
+    aspeo.py
+"""
+
+from asp import parse_toml
+import os
+import docopt
+
+PROJECT = parse_toml(os.path.join(os.path.dirname(__file__), "pyproject.toml"))
+VERSION = PROJECT["project"]["version"]
+
+if __name__ == "__main__":
+    docopt.docopt(__doc__)
+    print("ASPeo version: v{}".format(VERSION))

d961779549115e258c80db5338c5e0e6c7f22b23
LeoLetellier <leo.letellier.6@gmail.com>
2025-05-28 11:12:32 +0200

    Add toml options for pt
    * can use source suffix and prefix for constructing image path
    * can use own cameras instead of default tsai
    * add a preset for S2 pixel tracking pt_s2

--- asp_pt.py
+++ asp_pt.py
@@ -13,31 +13,64 @@ Options:
 """
 
 from asp import stereo, parse_toml, BLACK_LEFT, BLACK_RIGHT
+import os
 import docopt
 
 
-def make_pairs(pairs_file, dates, mp):
+def make_pairs(pairs_file: str, dates: list, mp: list, cams: list | None):
     """Derive pairing between images and dates based on a pair file"""
-    assos = dict(zip(dates, mp))
+    assos_mp = dict(zip(dates, mp))
     with open(pairs_file, "r") as infile:
         line = infile.read().split("\n")
         date_pairs = [p.split(" ") for p in list(filter(None, line))]
-    img_pairs = [[assos[p[0]], assos[p[1]]] for p in date_pairs]
-    return img_pairs, date_pairs
+    img_pairs = [[assos_mp[p[0]], assos_mp[p[1]]] for p in date_pairs]
+    if cams is None:
+        cam_pairs = len(dates) * [[BLACK_LEFT, BLACK_RIGHT]]
+    else:
+        assos_cam = dict(zip(dates, cams))
+        cam_pairs = [[assos_cam[p[0]], assos_cam[p[1]]] for p in date_pairs]
+    return img_pairs, date_pairs, cam_pairs
+
+
+def fetch_sources(params: dict):
+    """Fetch the id and image path from source
+    Enable composing the image path using the id and a prefix/suffix
+    """
+    sources = params["source"]
+    dates = []
+    mp = []
+    cams = []
+    prefix = params["aspeo"].get("src_prefix", "")
+    suffix = params["aspeo"].get("src_suffix", "")
+    for s in sources:
+        d = s["id"]
+        m = s.get("mp", None)
+        c = s.get("cam", None)
+        if m is None:
+            m = d
+        m = prefix + m + suffix
+        dates.append(d)
+        mp.append(m)
+        if cams is not None:
+            if c is None:
+                cams = None
+            else:
+                cams.append(c)
+
+    return dates, mp, cams
 
 
 def pixel_tracking(params: dict, debug=False):
     """Pixel tracking sequence using stereo"""
-    sources = params["source"]
     pairs_file = params["stereo"]["pairs"]
+    output_dir = params["aspeo"].get("output", ".")
 
-    dates = [s["id"] for s in sources]
-    mp = [s["mp"] for s in sources]
-    img_pairs, date_pairs = make_pairs(pairs_file, dates, mp)
+    dates, mp, cams = fetch_sources(params)
+    img_pairs, date_pairs, cam_pairs = make_pairs(pairs_file, dates, mp, cams)
 
-    for p, d in zip(img_pairs, date_pairs):
-        output = d[0] + "_" + d[1] + "/pt"
-        stereo(p, [BLACK_LEFT, BLACK_RIGHT], output, params, debug=debug)
+    for p, d, c in zip(img_pairs, date_pairs, cam_pairs):
+        output = os.path.join(output_dir, d[0] + "_" + d[1] + "/pt")
+        stereo(p, c, output, params, debug=debug)
 
 
 if __name__ == "__main__":
